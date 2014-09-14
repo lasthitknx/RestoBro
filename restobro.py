967a1c339137883cc7ed41b0a5c10f1ef61f545a
lasthitknx <lasthitknxx@gmail.com>
2014-09-14 16:55:24 +0300

    Update restobro.py

--- restobro.py
+++ restobro.py
@@ -49,9 +49,47 @@ class RestoBro():
 
     def parse_single_fight(self, pull_index, actor):
         log = open(self.log, encoding='utf-8').readlines()
+        tmp = open('tmp.txt', 'w', encoding='utf-8',)
         for line in range(self.fights[pull_index]['start_line'], self.fights[pull_index]['end_line']):
-            if actor in log[line]:
-                print(log[line])
+            if actor in log[line] or ENCOUNTER_START_MESSAGE in log[line] or ENCOUNTER_END_MESSAGE in log[line]:
+                tmp.write(log[line]+'\n')
+
+    def applied_without_harmony(self, pull_index, actor):
+        self.parse_single_fight(pull_index, actor)
+        log = open('tmp.txt', encoding='utf-8').readlines()
+        ranges = {}
+        current_range = {}
+        key = '0'
+        for line in log:
+            if ENCOUNTER_START_MESSAGE in line or 'Гармония' in line:
+                if line.split(',')[0].split(' ')[3] == 'SPELL_AURA_REMOVED':
+                    current_range['start'] = log.index(line)
+            if ENCOUNTER_END_MESSAGE in line or 'Гармония' in line:
+                if line.split(',')[0].split(' ')[3] == 'SPELL_AURA_APPLIED':
+                    current_range['end'] = log.index(line)
+                    ranges[key] = current_range
+                    key = str(int(key) + 1)
+                    current_range = {}
+        for r in ranges:
+            for line in range(ranges[r]['start'], ranges[r]['end']):
+                try:
+                    if log[line].split('  ')[1].split(',')[10] == '"Омоложение"':
+                        if log[line].split('  ')[1].split(',')[0] == 'SPELL_AURA_APPLIED':
+                            print(log[line])
+                    if log[line].split('  ')[1].split(',')[10] == '"Жизнецвет"':
+                        if log[line].split('  ')[1].split(',')[0] == 'SPELL_AURA_APPLIED':
+                            print(log[line])
+                    if log[line].split('  ')[1].split(',')[10] == '"Буйный рост"':
+                        if log[line].split('  ')[1].split(',')[0] == 'SPELL_CAST_SUCCESS':
+                            print(log[line])
+                    if log[line].split('  ')[1].split(',')[10] == '"Спокойствие"':
+                        if log[line].split('  ')[1].split(',')[0] == 'SPELL_CAST_SUCCESS':
+                            print(log[line])
+                except IndexError:
+                    pass
+
+
+
 
 
 
